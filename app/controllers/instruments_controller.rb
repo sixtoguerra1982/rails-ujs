6cc2e062e76f589334cac8a1244f592e5eac2f06
Sixto Guerra <sixto.guerra1982@gmail.com>
2022-01-19 22:49:33 -0300

    new version

--- app/controllers/instruments_controller.rb
+++ app/controllers/instruments_controller.rb
@@ -1,4 +1,6 @@
 class InstrumentsController < ApplicationController
+    before_action :set_instrument, only: [:edit, :update, :show, :destroy]
+
     def create
         @instrument = Instrument.new(instrument_params)
         unless @instrument.save
@@ -6,7 +8,29 @@ class InstrumentsController < ApplicationController
         end
     end
 
+    def edit
+        
+    end
+
+    def update
+        unless @instrument.update(instrument_params)
+            render json: @instrument.errors, status: :unprocessable_entity
+        end
+    end
+
+    def destroy
+        @instrument.destroy
+    end
+
+    def show
+
+    end
+
     private
+
+    def set_instrument
+        @instrument = Instrument.find(params[:id])
+    end
 
     def instrument_params
         params.require(:instrument).permit(:name)
--- app/models/instrument.rb
+++ app/models/instrument.rb
@@ -1,4 +1,8 @@
 class Instrument < ApplicationRecord
     validates :name, presence: true 
     validates :name, uniqueness: true
+
+    def to_s
+        self.name
+    end
 end
